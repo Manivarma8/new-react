bf1db279e611b923e6e11347e790efd5fea92254
Manivarman <122022651+Manivarma8@users.noreply.github.com>
2023-10-01 21:02:04 +0530

    first commit

--- src/del.jsx
+++ src/del.jsx
@@ -6,6 +6,8 @@ const Foot = (props)=>{
     <div id = "fish"className="dele m-4">
     <input placeholder="Titles" name = "title" value = {`${props.Title}`} type="text"></input>
     <textarea placeholder="Contents" name = "content" value = {props.content}  rows="10"></textarea>
+   <span className = "btns">
     <button onClick = {()=>props.Deletes(props.id)}>Del</button>
+    </span>
     </div>) }
 export default Foot;
